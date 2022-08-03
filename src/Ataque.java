e55781cf5cf23daad814cae637e0d1b69b49f9c6
Vicente Cruz <vicentecruzf@gmail.com>
2022-08-02 20:35:52 -0400

    agregue mas clases

---
+++ src/Ataque.java
@@ -0,0 +1,15 @@
+public class Ataque {
+
+    private int id;
+    private String nombre;
+    private int damage;
+    private int curacion;
+
+    Ataque(int id, String nombre, int damage, int curacion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.damage = damage;
+        this.curacion = curacion;
+    }
+    
+}
--- src/Personaje.java
+++ src/Personaje.java
@@ -1,3 +1,11 @@
 public class Personaje {
-    
+    private int id;
+    private String nombre;
+    private int [] ataques;
+
+    Personaje(int id, String nombre, int [] ataques){
+        this.id = id;
+        this.nombre = nombre;
+
+    }
 }
