5b3f76916f740b197df423b4d1c0fc5f615a8baf
Vicente Cruz <vicentecruzf@gmail.com>
2022-08-04 02:12:57 -0400

    la creacion de personaje y la creacion de habilidades estan creadas

--- src/Menu.java
+++ src/Menu.java
@@ -3,10 +3,7 @@ public class Menu {
     private BufferedReader buffer;
     private Personaje prueba;
     private int [] lista;
-     Ataque ataque1;
-     Ataque ataque2;
-     Ataque ataque3;
-     Ataque ataque4;
+     
 
      Ataque [] lista_ataques;
 
@@ -14,12 +11,9 @@ public class Menu {
     Menu(){
         buffer = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Probando Menu");
-        this.lista_ataques= new Ataque[]{ataque1,ataque2,ataque3,ataque4}; 
+         
         this.lista= new int[]{1, 2, 3, 4}; 
-        this.ataque1= new Ataque(1, "Primer golpe", 10, 0);
-        this.ataque2= new Ataque(2, "Segundo golpe", 20, 0);
-        this.ataque3= new Ataque(3, "Tercer golpe", 30, 0);
-        this.ataque4= new Ataque(4, "Cuarto golpe", 40, 0);
+        
 
 
 
@@ -32,11 +26,11 @@ public class Menu {
         int op;
         
         System.out.println("Seleccione las opciones que quiere utilizar: ");
-        System.out.println("1 ver el nombre del personaje, 2 ver el id del personaje: ");
+        System.out.println("1 ver el nombre de las habilidades del personaje , 2 ver el id del personaje: ");
         try{
             op= Integer.parseInt(buffer.readLine() );
             switch(op){
-                case 1: System.out.println("el nombre del personaje creado es: "+this.prueba.getNombre() );
+                case 1: System.out.println(Setup.perso1.devolverAtaques() );
             }
 
         }catch(IOException e){
--- src/Personaje.java
+++ src/Personaje.java
@@ -1,11 +1,16 @@
 public class Personaje {
     private int id;
     private String nombre;
-    private int [] ataques;
+    private Ataque [] ataques = new Ataque[4];
 
-    Personaje(int id, String nombre, int [] ataques){
+    Personaje(int id, String nombre, int [] list){
         this.id = id;
         this.nombre = nombre;
+
+        for (int i=0; i<list.length; i=i+1){
+            ataques[i] = buscador(list[i]);
+        }
+        
 
     }
 
@@ -18,5 +23,27 @@ public class Personaje {
         return id;
     }
 
-    
+    public Ataque buscador(int a)
+    {
+        Ataque ataque = new Ataque (0, " ", 0, 0); 
+
+        for(int i=0 ; i<SetupAtaques.listaAtaque.length ; i=i+1) {
+            if (a == SetupAtaques.listaAtaque[i].getId()){
+                ataque = SetupAtaques.listaAtaque[i];
+            }
+        }
+        
+        return ataque;
+
+    }
+    public Ataque [] getAtaques(){
+        return ataques;
+    }
+
+    public String devolverAtaques(){
+        String a;
+        a = ("Los ataques son: "+ ataques[0].getNombre() +" " +ataques[1].getNombre() +" " +ataques[2].getNombre() +" " +ataques[3].getNombre() );
+
+        return a;
+    }
 }
